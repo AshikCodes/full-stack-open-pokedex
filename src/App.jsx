b9331e4b5b1095f0259d4038951aeaf377ba8448
Ashik Reji <ashikreji27@gmail.com>
2022-08-27 02:55:44 +0400

    Completed 11.3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
+//done 11.3
 
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
