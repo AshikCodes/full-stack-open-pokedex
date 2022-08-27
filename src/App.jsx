9d9c66171ef59a28584c8312c234c5000cd87fc5
Ashik Reji <ashikreji27@gmail.com>
2022-08-27 16:10:45 +0400

    Completed 11.5

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-//done 11.4
+//done 11.5
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
