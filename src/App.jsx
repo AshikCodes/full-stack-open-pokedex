56dcafa46907d4c4bba849ead4d343dbc19d2f23
Ashik Reji <ashikreji27@gmail.com>
2022-08-29 01:14:29 +0400

    another one

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-//done assd
+//done assdads
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
