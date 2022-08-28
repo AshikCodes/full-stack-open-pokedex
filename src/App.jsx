2293ae8038e93949f35eaf2962765168acda36ae
Ashik Reji <ashikreji27@gmail.com>
2022-08-28 21:30:37 +0400

    10th attempt

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-//done asd
+//done assd
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
