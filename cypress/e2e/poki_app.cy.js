26768dd4a1959ef792d0b34798ceecc75b563adf
Ashik Reji <ashikreji27@gmail.com>
2022-08-28 15:11:58 +0400

    Another attempt at setting up cypress workflows

--- cypress/e2e/poki_app.cy.js
+++ cypress/e2e/poki_app.cy.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    /* eslint-disable no-undef */
-    cy.visit('http://localhost:5000/')
+    cy.visit('http://localhost:8080/', { timeout: 20000 })
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated', function() {
+    cy.visit('http://localhost:8080/')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
   })
 })
