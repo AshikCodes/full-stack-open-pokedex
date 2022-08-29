17352e32aefc786b9d5f87dfbf05a9887aaace50
Ashik Reji <ashikreji27@gmail.com>
2022-08-29 15:02:14 +0400

    tried to update again

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-//done experimental one
+//pls work
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
